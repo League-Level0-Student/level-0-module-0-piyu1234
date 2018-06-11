c21b443ab5aa5c3ffd3731053e8b75ce46711ced
amanddali <league@WTS22.attlocal.net>
2018-06-10 17:34:48 -0700

    practice

--- src/LastSunmmer.java
+++ src/LastSunmmer.java
@@ -1,14 +1,11 @@
 import javax.swing.JOptionPane;
 
 public class LastSunmmer {
-public static void main(String[] args) {
- String name = JOptionPane.showInputDialog(null, "What is your name?");
- String activity = JOptionPane.showInputDialog("What did you do last summer?");
-JOptionPane.showMessageDialog(null, "I know you did "+activity+" Muhahaha! " + name);
+	public static void main(String[] args) {
+		String name = JOptionPane.showInputDialog(null, "What is your name?");
+		String activity = JOptionPane.showInputDialog("What did you do last summer?");
+		JOptionPane.showMessageDialog(null,
+				"I know you did " + activity + " Muh" + name + " Hey " + name + " is so cool! I love you " + name);
 
-
-
-
-  
-}
+	}
 }
